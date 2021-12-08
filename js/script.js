470da9d7d07267223c82ec50193363333a859079
s.batalov <s.batalov@kokoc.tech>
2021-12-08 17:49:49 +0300

    init commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  const navbar = document.querySelector('.navbar');
+
+  function navbarScrolling(){
+    if(window.pageYOffset > 200){
+      navbar.classList.add('scrolling');
+    } else {
+      navbar.classList.remove('scrolling');
+    }
+  }
+  
+  navbarScrolling();
+
+  document.addEventListener("scroll", function(){
+    navbarScrolling();
+  })
+
+  document.addEventListener("click", function(e){
+    if(e.target.classList.contains('navbar-nav__mobile-burger')){
+      navbar.classList.toggle('is-open');
+    }
+    if(document.documentElement.clientWidth < 1100){
+      if(e.target.classList.contains('navbar-nav__link')){
+        navbar.classList.remove('is-open');
+      }
+    }
+  })
+
+});
